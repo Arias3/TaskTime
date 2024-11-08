9de852891872f7c77b7f0c5e7e66d69395308f24
anpateca <juanxdarias@gmail.com>
2024-11-08 02:15:38 -0500

    modified:   kivy_venv/Lib/site-packages/_distutils_hack/__pycache__/__init__.cpython-310.pyc 	modified:   kivy_venv/Lib/site-packages/win32/lib/__pycache__/pywin32_bootstrap.cpython-310.pyc 	modified:   kivy_venv/Scripts/activate 	modified:   kivy_venv/Scripts/activate.bat 	modified:   kivy_venv/pyvenv.cfg

--- kivy_venv/Scripts/activate.bat
+++ kivy_venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Juan\Documents\Proyecto\AppAngie\TaskTime\kivy_venv
+set VIRTUAL_ENV=C:\Users\rpina\OneDrive\Documentos\app\TaskTime\kivy_venv
 
 if not defined PROMPT set PROMPT=$P$G
 
